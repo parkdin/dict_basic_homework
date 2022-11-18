96c098710418e3ba54f3eaaafddb701663b5e741
parkdin <abdukokhorovizatillo00@gmail.com>
2022-11-18 19:08:05 +0900

    done

--- last_item.py
+++ last_item.py
@@ -3,4 +3,7 @@ def last_item():
     Given a dictionary, Return last item value.
     """
     data = {'a': 1, 'b': 2}
-    return
+    for i in data.values():
+        last = i
+    return last
+print(last_item())
